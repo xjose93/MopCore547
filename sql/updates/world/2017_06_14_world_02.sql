d7e896f61a53ebfa6e1fb5064559db566467f543
cooler-SAI <cooler19860212@yahoo.com>
2017-06-14 23:30:44 +0300

    More fixes from @alexkulya

--- sql/updates/world/2017_06_14_world_02.sql
+++ sql/updates/world/2017_06_14_world_02.sql
@@ -186,3 +186,20 @@ UPDATE `gossip_menu_option` SET `action_menu_id`=0 WHERE `menu_id`=12489;
 UPDATE `gossip_menu` SET `text_id`=17569 WHERE `entry`=12488;
 UPDATE `gossip_menu` SET `text_id`=17570 WHERE `entry`=12487;
 UPDATE `npc_text` SET `text0_0`="I... died?$b$bYes, you're right.  I died.  It was an orc... he cut off my hands, and left me to die.$b$b<Valdred looks down at his hands.>$b$bThese aren't my hands!  THESE AREN'T MY HANDS!" WHERE `ID`=17570;
+
+UPDATE `creature_template` SET `InhabitType`=4 WHERE `entry` IN (48624, 25063);
+UPDATE `creature_template_addon` SET `bytes1`=5, `bytes2`=1, `emote`=426 WHERE `entry`=8583;
+UPDATE `creature` SET `MovementType`=0,`spawndist`=0 WHERE `id` IN (44617, 7690, 39251);
+UPDATE `creature` SET `MovementType`=1,`spawndist`=2 WHERE `id` IN (3098);
+
+UPDATE `quest_template` SET `PrevQuestId`=0, `NextQuestId`=25126, `NextQuestIdChain`=25126 WHERE `Id`=25152;
+UPDATE `quest_template` SET `PrevQuestId`=25152, `NextQuestId`=25172, `NextQuestIdChain`=25172 WHERE `Id`=25126;
+UPDATE `quest_template` SET `PrevQuestId`=25126, `NextQuestId`=25127, `NextQuestIdChain`=25127 WHERE `Id`=25172;
+UPDATE `quest_template` SET `PrevQuestId`=25172, `NextQuestId`=0, `NextQuestIdChain`=0 WHERE `Id`=25127;
+
+UPDATE `quest_template` SET `PrevQuestId`=0, `NextQuestId`=25132, `NextQuestIdChain`=25132 WHERE `Id`=25131;
+UPDATE `quest_template` SET `PrevQuestId`=25131, `NextQuestId`=25133, `NextQuestIdChain`=25133 WHERE `Id`=25132;
+UPDATE `quest_template` SET `PrevQuestId`=25132, `NextQuestId`=25167, `NextQuestIdChain`=25167 WHERE `Id`=25133;
+UPDATE `quest_template` SET `PrevQuestId`=25133, `NextQuestId`=25168, `NextQuestIdChain`=25168 WHERE `Id`=25167;
+UPDATE `quest_template` SET `PrevQuestId`=25167, `NextQuestId`=25171, `NextQuestIdChain`=25171 WHERE `Id`=25168;
+UPDATE `quest_template` SET `PrevQuestId`=25168, `NextQuestId`=0, `NextQuestIdChain`=0 WHERE `Id`=25171;
