d5b95eaf66718b993d2d53e5435cca44bece15eb
Mysterium <mysterium@ice-wow.com>
2017-09-11 10:19:16 +0200

    Create 2017_09_11_world_00.sql

---
+++ sql/updates/world/2017_09_11_world_00.sql
@@ -0,0 +1,65 @@
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (61562, 74260, 48, 1, 0, 1, 1);
+
+-- add loot to npc 59788 --
+INSERT INTO creature_loot_template VALUES (59788, 94225, 0.19, 1, 0, 1, 1);
+
+-- add loot to npc 59788 --
+INSERT INTO creature_loot_template VALUES (59788, 94226, 0.12, 1, 0, 1, 1);
+
+-- add loot to npc 59788 --
+INSERT INTO creature_loot_template VALUES (59788, 94159, 0.12, 1, 0, 1, 1);
+
+-- add loot to npc 59788 --
+INSERT INTO creature_loot_template VALUES (59788, 94227, 0.1, 1, 0, 1, 1);
+
+-- add loot to npc 59788 --
+INSERT INTO creature_loot_template VALUES (59788, 94223, 0.09, 1, 0, 1, 1);
+
+-- add loot to npc 55438 --
+INSERT INTO creature_loot_template VALUES (55438, 94158, 0.03, 1, 0, 1, 1);
+
+-- add loot to npc 55438 --
+INSERT INTO creature_loot_template VALUES (55438, 94227, 0.03, 1, 0, 1, 1);
+
+-- add loot to npc 55438 --
+INSERT INTO creature_loot_template VALUES (55438, 74844, 1.9, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 72988, 3, 1, 0, 1, 2);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 81405, 1.4, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 74848, 1, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 81406, 0.6, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 81973, 0.5, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 81986, 0.4, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 89683, 0.2, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 120138, 0.17, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 82007, 0.03, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 81978, 0.03, 1, 0, 1, 1);
+
+-- add loot to npc 55195 --
+INSERT INTO creature_loot_template VALUES (55195, 81996, 0.01, 1, 0, 1, 1);
+
+-- add loot to npc 50828 --
+INSERT INTO creature_loot_template VALUES (50828, 86591, 10, 1, 0, 1, 1);
+
+-- delete double oondasta spawn
+DELETE FROM creature WHERE guid=344679;
